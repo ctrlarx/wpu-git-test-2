193c151b69ebc43877aa0beea00e5507d5e4ca45
ctrlarx <naufalbintangpradana@gmail.com>
2024-01-01 19:12:36 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores perspiciatis tempore commodi voluptatum, sunt libero nihil labore iusto voluptatem ratione maxime? Aspernatur iure deserunt nisi voluptatum debitis repellendus alias quis.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
